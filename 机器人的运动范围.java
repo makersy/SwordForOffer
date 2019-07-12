0dfb01fe901ba60187c1500f363f6c4d2ab69656
yhl <1352605699@qq.com>
2019-07-12 18:46:54 +0800

    commit 07/12

--- 机器人的运动范围.java
+++ 机器人的运动范围.java
@@ -1,43 +1,42 @@
 /*
- * 题目描述
-地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，每一次只能向左，右，上，下四个方向移动一格，但是不能进入行坐标和列坐标的数位之和大于k的格子。 例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
+ 题目描述
+地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，每一次只能向左，右，上，下四个方向
+移动一格，但是不能进入行坐标和列坐标的数位之和大于k的格子。 例如，当k为18时，机器人能够进入方格
+（35,37），因为3+5+3+7 = 18。但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人
+能够达到多少个格子？
 */
-
-
 public class Solution {
-    public int movingCount(int k, int rows, int cols)
+    
+    //使用标记数组记录已到达过的格子，递归遍历
+    public int movingCount(int t, int rows, int cols)
     {
-        if( rows<=0 || cols<=0 ) return 0;
-        boolean[] flag = new boolean[rows*cols];
-
-        if( k<0 ){
-            return 0;
-        }
-        return fun(k, rows, cols, 0, 0, flag);
+        if( t<0 || rows <= 0 || cols <= 0 ) return 0;
+        boolean[] flag = new boolean[rows*cols];  //已访问标记
+        return fun(t, 0, 0, rows, cols, flag);
     }
-
-    public int fun(int k, int rows, int cols, int x, int y, boolean[] flag){
-        if( x<0 || x>=rows || y<0 || y>=cols || flag[x*cols+y] ){
+    
+    //遍历. m 行数 n 列数
+    public int fun(int t, int x, int y, int m, int n, boolean[] flag){
+        if( getBitSum(x, y) > t || x>=n || x<0 || y>=m || y<0 ){
             return 0;
         }
-        if( get(x)+get(y) > k ){
-            return 0;
-        }
-        flag[x*cols+y] = true;
-        return 1 + fun(k, rows, cols, x-1, y, flag) + fun(k, rows, cols, x+1, y, flag) +
-                    fun(k, rows, cols, x, y-1, flag) + fun(k, rows, cols, x, y+1, flag);
+        if( flag[y*n+x] ) return 0;
+        
+        flag[y*n+x] = true;
+        return 1 + fun(t, x+1, y, m, n, flag) + fun(t, x, y+1, m, n, flag)
+            + fun(t, x-1, y, m, n, flag) + fun(t, x, y-1, m, n, flag);
     }
-
-    public int get(int num){
+    
+    public int getBitSum(int a, int b){
         int sum = 0;
-        while(num > 0){
-            sum += num%10;
-            num /= 10;
+        while( a > 0 ){
+            sum += a%10;
+            a /= 10;
+        }
+        while( b > 0 ){
+            sum += b%10;
+            b /= 10;
         }
         return sum;
     }
-
-    private int max(int a, int b, int c, int d){
-        return Math.max(Math.max(a, b), Math.max(c, d));
-    }
-}
+} 
--- 矩阵中的路径.java
+++ 矩阵中的路径.java
@@ -1,41 +1,46 @@
-/*题目描述
-请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，向右，向上，向下移动一个格子。如果一条路径经过了矩阵中的某一个格子，则之后不能再次进入这个格子。 例如 a b c e s f c s a d e e 这样的3 X 4 矩阵中包含一条字符串"bcced"的路径，但是矩阵中不包含"abcb"路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入该格子。
+/*
+   题目描述
+    请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。
+    路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，向右，向上，向下移动一个格子。
+    如果一条路径经过了矩阵中的某一个格子，则之后不能再次进入这个格子。 
+    例如 a b c e s f c s a d e e 这样的3 X 4 矩阵中包含一条字符串"bcced"的路径，但是矩
+    阵中不包含"abcb"路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能
+    再次进入该格子。
 */
 
 public class Solution {
+    
+    /*
+      使用一个bool型数组来映射此矩阵，记录是否被访问过。经过一个地方时就设置flag为true，代表
+      此位置是该路径所经过的地方，如果此路径不满足条件，就重置flag，尝试新的路径
+    */
     public boolean hasPath(char[] matrix, int rows, int cols, char[] str)
     {
-        if( matrix.length==0 || rows*cols != matrix.length){
-            return false;
-        }
-        boolean[] flag = new boolean[matrix.length];  //记录每个位置是否到达过，到达过的格子不能再经过
-        for( int i=0; i<rows; ++i ){
-            for( int j=0; j<cols; ++j ){
-                if( fun(matrix, rows, cols, i, j, 0, flag, str) ){
-                    return true;
-                }
+        if( matrix==null || rows*cols!=matrix.length ) return false;
+        boolean[] flags = new boolean[matrix.length];
+        for(int i=0; i<cols; i++ ){
+            for( int j=0; j<rows; j++ ){
+                if( fun(matrix, rows, cols, i, j, str, 0, flags) ) return true;
             }
         }
         return false;
     }
 
-    private boolean fun(char[] m, int rows, int cols, int x, int y, int idx, boolean[] flag, char[] str){
-        int index = x*cols+y; //计算str当前位置字符在matrix中位置
-        if( x<0 || x>=rows || y<0 || y>=cols || m[index]!=str[idx] || flag[index]){
-            return false;
-        }
-        if( idx==str.length-1 ){
-            //字符串全部对应
+    //x 横坐标 y 纵坐标
+    private boolean fun(char[] m, int rows, int cols, int x, int y, char[] str, int idx, boolean[] flags){
+        int index = y * cols + x;
+        if( x>=cols || x<0 || y>=rows || y<0 || m[index]!=str[idx] || flags[index] ) return false;
+        //如果字符串和矩阵全部对应
+        if( idx==str.length-1 ) return true;
+        flags[index] = true;
+        if( fun(m, rows, cols, x+1, y, str, idx+1, flags) ||
+          fun(m, rows, cols, x, y+1, str, idx+1, flags) ||
+          fun(m, rows, cols, x-1, y, str, idx+1, flags) ||
+          fun(m, rows, cols, x, y-1, str, idx+1, flags) ){
             return true;
         }
-        flag[index] = true; //将当前位置设置为到达过
-        if ( fun(m, rows, cols, x-1, y, idx+1, flag, str) ||
-          fun(m, rows, cols, x+1, y, idx+1, flag, str) ||
-          fun(m, rows, cols, x, y-1, idx+1, flag, str) ||
-          fun(m, rows, cols, x, y+1, idx+1, flag, str) ){
-            return true;
-        }
-        flag[index] = false;  //如果从当前位置出发没有满足条件路径，就设置为未到达过，防止影响遍历其他位置
+        //如果从当前位置出发任何方向都不行，那么重置flag访问记录
+        flags[index] = false;
         return false;
     }
 
